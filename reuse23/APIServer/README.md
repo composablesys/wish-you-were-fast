c4ea8634d8056836830f42f75158e98169b1a4b1
Alexa Halim <ah103@wellesley.edu>
2023-07-11 12:50:43 -0400

    Clean up app.py, add CSS, and create README

---
+++ reuse23/APIServer/README.md
@@ -0,0 +1,4 @@
+#API Server
+
+## How to set up
+1. install flask, flask_restx, dateutil.parser, psycopg2, json, fnmatch, math
--- reuse23/APIServer/app.py
+++ reuse23/APIServer/app.py
@@ -36,11 +36,6 @@ def set_cors(response):
    response.headers.add("Access-Control-Allow-Origin", "*")
    return response
 
-@api.route('/check_status')
-class statusCheck(Resource):
-   def get(self):
-      return {"data":1}
-
 configs = ['int', 'jit', 'nok', 'nokfold','noisel','nomr']
 suites = ['ostrich', 'libsodium', 'polybench']
 # total_time metric
@@ -66,6 +61,11 @@ class getExecution(Resource):
       conn.close()
       return results
    
+''' faulty version to render home page '''
+@app.route("/", methods=['GET'])
+def index():
+   return render_template('index4.html', engine="jsc")
+
 ''' working version of main graph with interactive line highlight '''
 @app.route('/main1')
 def home1():
@@ -77,7 +77,7 @@ def home1():
 def home3():
    return render_template('index3.html')
 
-# version to use selections to filter data in another graph through changing url
+# working version to use selections to filter data in another graph through changing url
 @app.route('/main2/<string:engine>')
 def home2(engine):
    return render_template('index2.html', engine=engine)
@@ -195,69 +195,6 @@ class getAllMean(Resource):
               "Version": datum[4],"Machine": datum[6], "MetricType": datum[7], "Value": datum[8]}
        response.append(tup)
     return {"data": response}
-
-@api.route('/get_bw_dates')
-class getBwDates(Resource):
-   def get(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('startDate', type=str, required=True)
-    parser.add_argument('endDate', type=str, required=True)
-    args = parser.parse_args()
-
-    try:
-       if parse(args['startDate']) <= parse(args['endDate']):
-            print(args['startDate'])
-            print(args['endDate'])
-       else: 
-          raise Exception
-    except:
-       return "Improper date range - please ensure startDate and endDate to be in yyyy-mm-dd format", 400
-
-
-    conn = psycopg2.connect(database=db,user=user,password=pwd)
-    cur = conn.cursor()
-
-    cur.execute('''SELECT exp_date, benchmark_suite, benchmark_item, engine, version, config,
-                machine, metric_type, avg FROM summary WHERE exp_date BETWEEN %s AND %s
-                AND exp_label=%s''',(args['startDate'], args['endDate'], nightlyRunID,))
-    data = cur.fetchall()
-    response = []
-    for datum in data:
-       tup = {"Date": datum[0].isoformat(), "Suite": datum[1], "BenchmarkItem": datum[2], "Engine": datum[3].rstrip().lstrip()+'-'+datum[5].rstrip().lstrip(),
-              "Machine": datum[6], "MetricType": datum[7], "Value": datum[8]}
-       response.append(tup)
-
-    return {"data": response}
-
-@api.route('/get_percentiles')
-class getPerc(Resource):
-   def get(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('date', type=str, required=True)
-    parser.add_argument('benchmark_suite', type=str, required=True)
-    args = parser.parse_args()
-
-    try:
-       parse(args['date'])
-    except:
-       return "Improper date range - please ensure date to be in yyyy-mm-dd format", 400
-
-
-    conn = psycopg2.connect(database=db,user=user,password=pwd)
-    cur = conn.cursor()
-
-    cur.execute('''SELECT percentile_95, min, max, metric_type, engine, version, config, machine 
-               FROM summary WHERE exp_date=%s AND exp_label=%s AND benchmark_suite = %s''',
-               (args['date'],nightlyRunID,args['benchmark_suite']))
-
-    
-    data = cur.fetchall()
-    response = []
-    for datum in data:
-       response.append(list(datum))
-
-    return {"data": response}
-
 
 if __name__ == "__main__":
    app.run(host='grammont.lan.local.cmu.edu',port=6363, debug=True)
